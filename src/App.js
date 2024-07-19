a1e750b86557ab8abb746cb2283a0f1ee44373a6
Rohit-huta-p <vcrohithuta.p@gmail.com>
2024-07-19 17:08:39 +0530

    link scroll updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Main from './Home/Main';
 import Navbar from './Home/Navbar';
 import AboutUs from './Home/AboutUs';
 import Activities from './Home/Activities';
-import Gallery from './Home/Gallery'
+import Gallery from './Home/Gallery';
 import ContactForm from './Home/ContactForm';
 import Footer from './Home/Footer';
+import { Element } from 'react-scroll';
+
 function App() {
   return (
     <div className="bg-slate-50">
       <Navbar />
       <div className='p-2'>
-        <Main />
-        <AboutUs />
-        <Activities />
-        <Gallery />
-        <ContactForm />
+        <Element name="main">
+          <Main />
+        </Element>
+        <Element name="about-us">
+          <AboutUs />
+        </Element>
+        <Element name="activities">
+          <Activities />
+        </Element>
+        <Element name="gallery">
+          <Gallery />
+        </Element>
+        <Element name="contact-form">
+          <ContactForm />
+        </Element>
         <Footer />
       </div>
     </div>
--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -1,96 +1,101 @@
-import { useState } from "react"; 
-import logo from '../assets/images/logo_school.png'
+import React, { useState } from "react";
+import logo from '../assets/images/logo_school.png';
+import { Link } from 'react-scroll';
+
 export default function Navbar() {
-  const [isNavOpen, setIsNavOpen] = useState(false); 
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <>
-    <div className="bg-slate-700 text-white">
+      <div className="bg-slate-700 text-white">
         <h3>Working Hours</h3>
-    </div>
-    <div className="flex items-center justify-between border-b border-gray-400">
-      <a href="/" className="flex items-center">
-        <img src={logo} alt="logo" className="w-[5rem]"/>
-        <p className="text-sm font-normal">Rising superstar school</p>
-      </a>
-      <nav>
-        <section className=" flex lg:hidden">
-          <div
-            className="space-y-1.5 mr-3"
-            onClick={() => setIsNavOpen((prev) => !prev)}
-          >
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-          </div>
-
-          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}> 
+      </div>
+      <div className="flex items-center justify-between border-b border-gray-400">
+        <a href="/" className="flex items-center">
+          <img src={logo} alt="logo" className="w-[5rem]" />
+          <p className="text-sm font-normal">Rising superstar school</p>
+        </a>
+        <nav>
+          <section className=" flex lg:hidden">
             <div
-              className="absolute top-10 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)} 
+              className="space-y-1.5 mr-3"
+              onClick={() => setIsNavOpen((prev) => !prev)}
             >
-              <svg
-                className="h-7 w-8 text-gray-600"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
             </div>
-            <ul className="flex flex-col items-center justify-between min-h-[250px]">
-              <li className="border-b border-gray-300 my-8 uppercase">
-                <a href="/about">About</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/portfolio">Activites</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/contact">Gallery</a>
-              </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/contact">Contact Form</a>
-              </li>
-            </ul>
-          </div>
-        </section>
 
-        <ul className=" hidden space-x-3 lg:flex">
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-    </div>
+            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+              <div
+                className="absolute top-10 right-0 px-8 py-8"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <svg
+                  className="h-7 w-8 text-gray-600"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                <li className="border-b border-gray-300 my-8 uppercase">
+                  <Link to="about-us" smooth={true} duration={500} onClick={() => setIsNavOpen(false)}>About</Link>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <Link to="activities" smooth={true} duration={500} onClick={() => setIsNavOpen(false)}>Activities</Link>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <Link to="gallery" smooth={true} duration={500} onClick={() => setIsNavOpen(false)}>Gallery</Link>
+                </li>
+                <li className="border-b border-gray-400 my-8 uppercase">
+                  <Link to="contact-form" smooth={true} duration={500} onClick={() => setIsNavOpen(false)}>Contact Form</Link>
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <ul className="hidden space-x-3 lg:flex">
+            <li>
+              <Link to="about-us" smooth={true} duration={500}>About</Link>
+            </li>
+            <li>
+              <Link to="activities" smooth={true} duration={500}>Activities</Link>
+            </li>
+            <li>
+              <Link to="gallery" smooth={true} duration={500}>Gallery</Link>
+            </li>
+            <li>
+              <Link to="contact-form" smooth={true} duration={500}>Contact Form</Link>
+            </li>
+          </ul>
+        </nav>
+        <style>{`
+          .hideMenuNav {
+            display: none;
+          }
+          .showMenuNav {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            background: white;
+            z-index: 10;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            align-items: center;
+          }
+        `}</style>
+      </div>
     </>
   );
 }
